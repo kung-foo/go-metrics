f95d5b131774ee431027835e8d21e6546688b317
kung-foo <github@jonathan.camp>
2015-05-31 11:41:18 +0200

    add some comments

--- examples/cpu_window.go
+++ examples/cpu_window.go
@@ -9,21 +9,25 @@ import (
 )
 
 func main() {
+	// setup windows for a sample rate of 2Hz
 	cpu5 := metrics.NewWindowSink(time.Second*5, 10)
 	cpu15 := metrics.NewWindowSink(time.Second*15, 30)
 	cpu60 := metrics.NewWindowSink(time.Second*60, 120)
 
+	// use fanout sink to write to all three windows
 	cpuSink := &metrics.FanoutSink{cpu5, cpu15, cpu60}
 
 	key := []string{"cpu"}
 
 	go func() {
+		// collect CPU utilization every 500ms
 		for _ = range time.Tick(time.Millisecond * 500) {
 			v, _ := cpu.CPUPercent(0, false)
 			cpuSink.AddSample(key, float32(v[0]))
 		}
 	}()
 
+	// wait for first samples to come in
 	time.Sleep(time.Second * 1)
 
 	for _ = range time.Tick(time.Second * 1) {
